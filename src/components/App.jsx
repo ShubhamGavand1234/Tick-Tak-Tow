273b3126f0f77d345119492693fc5f4ec7d0e6a9
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-08-25 21:58:40 +0530

    GameBoard Structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./Player";
+import GameBoard from "./GameBoard";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
             <Player name="Player 1" symbol="X" />
             <Player name="Player 2" symbol="O" />
           </ol>
-          Game Board
+          <GameBoard />
         </div>
         Log
       </main>
